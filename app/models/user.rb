20e807c3f8f90dc03e2e8be44edd6fceab6c427e
daikadicode <tien.vu@backvietnam.com>
2017-11-13 21:50:36 +0700

    add admin/user controller start making admin layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  enum role: [:admin, :client]
+  enum role: [:admin, :user]
 end
