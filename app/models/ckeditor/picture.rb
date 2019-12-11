7a8c917243d2b0e789906db586a3d9ff24688ff0
Vu Tien <tien7668@gmail.com>
2019-12-11 14:41:26 +0700

    .

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -1,6 +1,6 @@
 class Ckeditor::Picture < Ckeditor::Asset
   mount_uploader :data, CkeditorPictureUploader, mount_on: :data_file_name
-
+  self.inheritance_column = nil
   def url_content
     url(:content)
   end
