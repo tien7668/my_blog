9c014dd32cd32c2280078bc771779ddce6c8f5b2
tien7668 <tien7668@gmail.com>
2020-06-24 10:22:46 +0700

    .

--- app/services/create_admin.rb
+++ app/services/create_admin.rb
@@ -1,11 +1,11 @@
 class CreateAdmin
   def call
-    user = User.find_or_create_by!(email: Rails.application.secrets.admin_email) do |user|
-      user.password = Rails.application.secrets.admin_password
-      user.password_confirmation = Rails.application.secrets.admin_password
-      user.name = Rails.application.secrets.admin_name
-      user.username = Rails.application.secrets.admin_name
-      user.slug = Rails.application.secrets.admin_name
+    user = User.find_or_create_by!(email: ENV.fetch("admin_email")) do |user|
+      user.password = ENV.fetch("admin_password")
+      user.password_confirmation = ENV.fetch("admin_password")
+      user.name = ENV.fetch("admin_name")
+      user.username = ENV.fetch("admin_name")
+      user.slug = ENV.fetch("admin_name")
       user.role = :admin
     end
   end
