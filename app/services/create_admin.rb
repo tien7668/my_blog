8b728bf53b558890e059958237f1fd13b631fb98
tien7668 <tien7668@gmail.com>
2020-06-24 10:26:31 +0700

    .

--- app/services/create_admin.rb
+++ app/services/create_admin.rb
@@ -1,6 +1,6 @@
 class CreateAdmin
   def call
-    user = User.find_or_create_by!(email: ENV.fetch("admin_email")) do |user|
+    user = User.find_or_create_by!(email: ENV.fetch("admin_name")) do |user|
       user.password = ENV.fetch("admin_password")
       user.password_confirmation = ENV.fetch("admin_password")
       user.name = ENV.fetch("admin_name")
