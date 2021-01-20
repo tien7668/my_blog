ff5cea27296fbd558d129438d997d3ba05858b0c
tien7668 <tien7668@gmail.com>
2021-01-20 10:34:32 +0700

    update pagination

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -18,11 +18,11 @@ class HomeController < ApplicationController
     per_page = 6
     @page = params["page"].to_i
     @posts = Post.order("created_at desc").limit(per_page).offset((@page-1) * per_page)
-    pages_count = Post.all.size / per_page + (Post.all.size % per_page == 0 ? 0 : 1)
+    @pages_count = Post.all.size / per_page + (Post.all.size % per_page == 0 ? 0 : 1)
     if @page == 1 || @page == 2
-      @pages = (1.. (pages_count < 5 ? pages_count : 5)).to_a
-    elsif @page == pages_count || @page == pages_count -1
-      @pages = ((pages_count < 5 ? 1 : pages_count - 4 ) .. pages_count).to_a
+      @pages = (1.. (@pages_count < 5 ? @pages_count : 5)).to_a
+    elsif @page == @pages_count || @page == @pages_count -1
+      @pages = ((@pages_count < 5 ? 1 : @pages_count - 4 ) .. @pages_count).to_a
     elsif 
       @pages = ((@page - 2) .. (@page+2)).to_a
     end 
